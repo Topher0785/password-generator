eaed76aa7b48acaca5997745f7a8e217b2834859
christopher dale <csethdale@live.com>
2021-11-26 18:12:28 -0600

    product with debuggers

--- style.js
+++ style.js
@@ -1,43 +1,49 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 function generatePassword(){
+debugger
+var passwordlength= prompt("Enter Password Character greater than 7 and less than 129");
+var number= confirm ("Do you want numbers in your password?");
+var special= confirm ("Do you want special characters?");
+var bigletter= confirm ("Do you want to add upper case characters?");
+var littleletter= confirm ("Do you want to add lower case characters?");
 
 
-
-
-var passwordlength= prompt("Enter Password Character less than 8 and greater than 128");
-if(passwordlength > 7 && passwordlength < 129){
-  var number= confirm("Do you want numbers in your password?");
-  var special= confirm("Do you want special characters?");
-  var bigletter= confirm("Do you want to add upper case characters?");
-  var littleletter= confirm("Do you want to add lower case characters?");
-}
+debugger
 
 // Create Array
-var numbchoice = ["0","1","2","3","4","5","6","7","8","9","0"];
+var numbchoice = ["0","1","2","3","4","5","6","7","8","9"];
 var specialcharacter = ["!","@","#","$","%","^","&","*","(",")","_","-","=","+","?","/"];
-var bigletter = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
-var littleletter =["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+var uppercaseletter= ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
+var lowercaseletter = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
 
-//Conditions for Arry to work
+var resultsArray = [];
+var userInputArray = [];
+
+
+
+//Conditions for Array to work
 if (special) {
   resultsArray=resultsArray.concat(specialcharacter);
 }
 if (number) {
-  resultsArray=resultsArray.concat(numchoice);
+  resultsArray=resultsArray.concat(numbchoice);
 }
 if (bigletter) {
-  resultsArray=resultsArray.concat(uppercharacter);
+  resultsArray=resultsArray.concat(uppercaseletter);
 }
 if (littleletter) {
-  resultsArray=resultsArray.concat(lowercharacter);
+  resultsArray=resultsArray.concat(lowercaseletter);
 }
-console.log(resultsArray);
-for (var i = 0; i < passwordlength; i++) {
-  userInputArray.push (resultsArray[Math.floor(Math.random() * resultsArray.length)]); 
+console.log(resultsArray)
+
+for (var i=0; i < passwordlength; i++) {
+userInputArray.push (resultsArray[Math.floor(Math.random() * resultsArray.length)]);
   }
 return userInputArray.join("") ;
 }
+
+debugger
 
 // Write  #password input
 function writePassword() {
